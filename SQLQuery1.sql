b2514c8572b0133cc29db4337586228fa2d7c1d2
CrystalxBlade <satish141m@gmail.com>
2026-08-27 23:12:06 +0530

    CRUD

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -22,3 +22,30 @@ grade INT
 -- Check existing table
 
 EXEC sp_help 'student'
+
+-- Inserting data 
+
+INSERT INTO student(student_id, name, age, grade)
+VALUES (101, 'Crystal', 23, 10)
+
+INSERT INTO student(student_id, name, age, grade)
+VALUES (102, 'Blade', 24, 11), (103, 'Knight', 25, 12)
+
+INSERT INTO student
+VALUES (104, 'Hornet', 26, 14)
+
+-- Reading Data
+
+SELECT * FROM student
+SELECT name FROM student
+
+-- Update 
+
+UPDATE student
+SET grade = 15
+WHERE student_id = 103
+
+-- Delete Data
+
+DELETE FROM student
+WHERE student_id = 104
